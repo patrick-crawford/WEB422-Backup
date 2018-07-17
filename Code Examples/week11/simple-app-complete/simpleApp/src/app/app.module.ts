2ee25a651102254ea126d27bc4795403c7c975dc
Patrick Crawford <patrick.crawford@senecacollege.ca>
2018-07-17 09:45:01 -0400

    Added simpleApp under simple-app-complete (incomplete)

---
+++ Code Examples/week11/simple-app-complete/simpleApp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    VehiclesComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
