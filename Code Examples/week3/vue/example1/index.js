47bc9c60f816da91258e433710d35374a085e0b6
David Humphrey <david.humphrey@senecacollege.ca>
2019-05-21 10:38:06 -0400

    Add vue examples

---
+++ Code Examples/week3/vue/example1/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import App from './components/App.vue';
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App></App>'
+});
---
+++ Code Examples/week3/vue/example2/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import App from './components/App.vue';
+
+const vm = new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  template: '<App></App>' 
+});
