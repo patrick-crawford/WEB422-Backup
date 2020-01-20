4f3b04365f361cb37843f4551af9b3bb097460ff
Patrick Crawford <patrick.crawford@senecacollege.ca>
2020-01-20 10:38:25 -0500

    added week 3 code example

---
+++ Code Examples/week3/README.md
@@ -0,0 +1,11 @@
+## Week 3 code examples
+
+This is the React starter app with our added &lt;Hello /&gt; and &lt;Clock /&gt; componnts added.
+
+Remember to run this command while in the directory/folder that has the `package.json` file:
+
+```
+npm i
+```
+
+<br>
---
+++ Code Examples/week3/my-app/src/Hello.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+function Hello() {
+
+    const user = {
+        firstName: 'Harper',
+        lastName: 'Perez'
+    };
+
+    return (
+        <p>Hello, {formatName(user)}!</p>
+    );
+}
+
+export default Hello;
