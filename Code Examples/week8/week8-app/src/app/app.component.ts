7e6454f3696e2f27e2d800611b9cda8f0a27ef0d
Patrick Crawford <patrick.crawford@senecacollege.ca>
2020-03-01 23:14:10 -0500

    updated week 8 example

--- Code Examples/week8/week8-app/src/app/app.component.ts
+++ Code Examples/week8/week8-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataManagerService } from './data-manager.service'
 import { Post } from './Post';
 
@@ -7,13 +7,21 @@ import { Post } from './Post';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   staticPost: Post;
+  posts: Array<Post>;
+
+  private livePostsSub; // make the subscription reference "private"
 
   constructor(private data: DataManagerService){ }
 
   ngOnInit(){
-    this.staticPost = this.data.getStaticPost()
+    this.staticPost = this.data.getStaticPost();
+    this.livePostsSub = this.data.getLivePosts().subscribe(data => this.posts = data);
+  }
+
+  ngOnDestroy(){
+    this.livePostsSub.unsubscribe();
   }
 }
--- Code Examples/week8/week8-app/src/app/data-manager.service.ts
+++ Code Examples/week8/week8-app/src/app/data-manager.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Post } from './Post';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataManagerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getStaticPost(): Post{ // return type "Post"
     return {
@@ -15,5 +17,9 @@ export class DataManagerService {
       title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
       body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
     }
+  }
+
+  getLivePosts(): Observable<Post[]> {
+    return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
   }
 }
