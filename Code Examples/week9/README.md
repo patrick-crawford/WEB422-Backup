ef4bd7f4eab08016d91f675055b7b57ed7be1f94
Patrick Crawford <patrick.crawford@senecacollege.ca>
2020-03-08 21:59:45 -0400

    week 9 example

---
+++ Code Examples/week9/README.md
@@ -0,0 +1,11 @@
+## Week 9 code example
+
+This is our fourth Angular code example. It illustrates how template-driven forms function within Angular, including using built in form validation and status.
+
+Once again, to ensure that this example functions, please remember to run the command while in the directory/folder that has the `package.json` file:
+
+```
+npm i
+```
+
+<br>
---
+++ Code Examples/week9/week9-app/src/app/driver/driver.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+
+
+export class Driver{
+    name: string; 
+    description: string; 
+    ownedTransportation: string[]; 
+    favouriteTransportation: string; 
+    driverLicence: boolean; 
+    vehicleUse: string; 
+}
+
+export class Option{
+  value: string;
+  text: string;
+}
+
+@Component({
+  selector: 'app-driver',
+  templateUrl: './driver.component.html',
+  styleUrls: ['./driver.component.css']
+})
+export class DriverComponent implements OnInit {
+
+  constructor() { }
+ 
+  // the data that will be used in the form
+  driverData: Driver;
+
+  // Define the preset list of "transportation" options
+  transportationList: Option[] = [
+    {value: "C", text: "Car"},
+    {value: "B", text: "Bus"},
+    {value: "M", text: "Motorcycle"},
+    {value: "H", text: "Helicopter"}
+  ];
+
+  ngOnInit() {
+
+    // Populate the "driverData" with some static data (this would normally come from a data service)
+    this.driverData = {
+      name: "Richard Hammond",
+      description: "Richard is a motor vehicle enthusiast",
+      ownedTransportation: ["C", "M"], 
+      favouriteTransportation: "M",
+      driverLicence: true, 
+      vehicleUse: "pleasure"
+    };
+    
+  }
+
+  onSubmit(f: NgForm): void { 
+
+    // output the form information to the console
+    console.log(f);
+
+  }
+
+}
