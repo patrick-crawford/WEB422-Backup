73fc69cc6039e03c682e2fc31bdc2f20bcd1629e
Patrick Crawford <patrick.crawford@senecacollege.ca>
2020-08-31 13:39:43 -0400

    update wee4 for ex to use hooks

--- Code Examples/week4/my-app/src/Product.js
+++ Code Examples/week4/my-app/src/Product.js
@@ -1,59 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link, Redirect} from 'react-router-dom';
 
-class Product extends React.Component{
-    
-    constructor(props){
-        super(props);
-        this.state = {
-            product: {},
-            loading: true,
-            found: false
-        }
-    }
+function Product(props){
 
-    componentDidMount(){
+    const [product, setProduct] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [found, setFound] = useState(false);
 
-        fetch(`https://reqres.in/api/unknown/${this.props.id}`)
+    useEffect(()=>{
+        fetch(`https://reqres.in/api/unknown/${props.id}`)
         .then(res=>res.json())
-        .then(data => {
-            if(data.data){ // a "data" property exists on the returned data
-                this.setState({
-                    product: data.data,
-                    found: true, // we found some data
-                    loading: false // no longer loading
-                });
+        .then(result => {
+            if(result.data){ // a "data" property exists on the returned data
+                setProduct(result.data);
+                setFound(true); // we found some data
+                setLoading(false); // no longer loading
             }else{
-                this.setState({
-                    found: false, // we did not find data
-                    loading: false // no longer loading
-                });
+                setFound(false); // we did not find any data
+                setLoading(false); // no longer loading
             }
-        })
-    }
+        });
+    }, [props.id]); // rerun this effect if props.id changes
 
-    render(){
-        if(this.state.loading){
-            return null; // could have a loading spinner, etc here
+    if(loading){
+        return null; // could have a loading spinner, etc here
+    }else{
+        if(found){
+            return (
+                <div>
+                    <h1>Product: {product.name}</h1>
+                    <br />
+                    <ul className="list-group">
+                        <li className="list-group-item"><strong>Name: </strong> {product.name}</li>
+                        <li className="list-group-item"><strong>Year: </strong> {product.year}</li>
+                        <li className="list-group-item"><strong>Color: </strong> {product.color}</li>
+                        <li className="list-group-item"><strong>Pantone Value: </strong> {product.pantone_value}</li>
+                    </ul>
+    
+                    <Link className="btn btn-primary" to="/Products">All Products</Link>
+                </div>
+            );
         }else{
-            if(this.state.found){
-                return (
-                    <div>
-                        <h1>Product: {this.state.product.name}</h1>
-                        <br />
-                        <ul className="list-group">
-                            <li className="list-group-item"><strong>Name: </strong> {this.state.product.name}</li>
-                            <li className="list-group-item"><strong>Year: </strong> {this.state.product.year}</li>
-                            <li className="list-group-item"><strong>Color: </strong> {this.state.product.color}</li>
-                            <li className="list-group-item"><strong>Pantone Value: </strong> {this.state.product.pantone_value}</li>
-                        </ul>
-        
-                        <Link className="btn btn-primary" to="/Products">All Products</Link>
-                    </div>
-                );
-            }else{
-                return <Redirect to={{ pathname: "/notFound"}} />
-            }
+            return <Redirect to={{ pathname: "/notFound"}} />
         }
     }
 }
--- Code Examples/week4/my-app/src/Products.js
+++ Code Examples/week4/my-app/src/Products.js
@@ -1,45 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 
-class Products extends React.Component{
-    
-    constructor(props){
-        super(props);
-        this.state = {
-            products: [],
-            loading: true
-        }
-    }
+function Products(props){
 
-    componentDidMount(){
+    const [loading, setLoading] = useState(true);
+    const [products, setProducts] = useState([]);
 
+    useEffect(()=>{
         fetch("https://reqres.in/api/unknown")
         .then(res=>res.json())
-        .then(data => {
-            this.setState({
-                products: data.data,
-                loading: false  // no longer loading
-            })
+        .then(result => {
+            setProducts(result.data);
+            setLoading(false);
         })
-    }
+    }, []);
 
-    render(){
-        if(this.state.loading){
-            return null; // could have a loading spinner, etc here
-        }else{
-            return (
-                <div>
-                    <h1>Products</h1>
-                    <br />
-                    <div className="list-group">
-                        {this.state.products.map((prod)=>{
-                            return <Link key={prod.id} className="list-group-item" style={{backgroundColor: prod.color}} to={`/Product/${prod.id}`}>{prod.name}: {prod.year}</Link>
-                        })}
-                    </div>
+    if(loading){
+        return null; // could have a loading spinner, etc here
+    }else{
+        return (
+            <div>
+                <h1>Products</h1>
+                <br />
+                <div className="list-group">
+                    {products.map((prod)=>{
+                        return <Link key={prod.id} className="list-group-item" style={{backgroundColor: prod.color}} to={`/Product/${prod.id}`}>{prod.name}: {prod.year}</Link>
+                    })}
                 </div>
-            );
-        }
+            </div>
+        );
     }
-};
+}
 
 export default Products;
